d1bfb6ab7ca05554ff62af2717e45db74937ccf6
Matthew Beale <matt.beale@addepar.com>
2021-07-02 09:59:12 -0400

    Remove toolbar, though pastefromword forces it. Drop about

--- dev/builder/build-config.js
+++ dev/builder/build-config.js
@@ -37,7 +37,7 @@ var CKBUILDER_CONFIG = {
 	],
 	plugins: {
 		a11yhelp: 0,
-		about: 1,
+		about: 0,
 		basicstyles: 1,
 		bidi: 0,
 		blockquote: 0,
@@ -93,7 +93,7 @@ var CKBUILDER_CONFIG = {
 		tableselection: 0,
 		tabletools: 0,
 		templates: 0,
-		toolbar: 1,
+		toolbar: 0,
 		undo: 1,
 		uploadimage: 0,
 		wysiwygarea: 1
